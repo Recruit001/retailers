3ca5b11efb2ade1064d091587763f05d692187d2
cool <923738685@qq.com>
2021-12-09 15:33:43 +0800

    完成数据分析页面的开发

--- src/views/Reports/Reports.vue
+++ src/views/Reports/Reports.vue
@@ -8,16 +8,69 @@
     </el-breadcrumb>
     <!-- 卡片区域 -->
     <el-card>
-      123
+      <div id="echars-table"></div>
     </el-card>
   </div>
 </template>
 
 <script>
+  // 导入echars
+  import * as echarts from 'echarts'
   export default {
-    name: 'Reports'
+    name: 'Reports',
+    data(){
+      return {
+        options: {},
+        options2: {
+          title: {
+            text: '用户来源'
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label: {
+                backgroundColor: '#E9EEF3'
+              }
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [
+            {
+              boundaryGap: false
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value'
+            }
+          ]
+        }
+      }
+    },
+    methods:{
+      async getOptionData(){
+        const {data: res} = await this.$getApi('reports/type/1')
+        this.options = {...this.options2,...res}
+      }
+    },
+    async mounted(){
+      let myChart = echarts.init(document.querySelector('#echars-table'))
+      const {data: res} = await this.$getApi('reports/type/1')
+      this.options = {...this.options2,...res}
+      myChart.setOption(this.options)
+    }
   }
 </script>
 
 <style lang = 'less' scoped >
+  #echars-table{
+    width:600px;
+    height:400px;
+  }
 </style>
